565db54ac8d27ede6e97d751b3838dbe7957574d
k-quinlan <132873356+k-quinlan@users.noreply.github.com>
2023-05-12 22:06:28 +1000

    Draft exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,38 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toRemove) {
+    //subtractArray = Array.from(toRemove);
 
+   // toRemove.map((args) => {
+
+   // });
+
+    let outputArray = inputArray.filter(x => !toRemove.includes(x));
+        
+/*        
+        {
+
+    });
+
+
+
+    let subLength = subtractArray.length;
+    for (let i = 0; i < subLength; i++) {
+        //find index
+
+    }
+    
+    let outputArray = inputArray.slice(index 1, 2, 3 etc)
+
+
+
+*/
+
+
+    return outputArray;
 };
+
+
+
+removeFromArray([9, 7, "E", 4], "E", 7)
 
 // Do not edit below this line
 module.exports = removeFromArray;
