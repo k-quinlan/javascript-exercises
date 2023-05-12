e70f9451f6726acb8f844828f369d3c23334324c
k-quinlan <132873356+k-quinlan@users.noreply.github.com>
2023-05-12 20:49:18 +1000

    Exercises 1 and 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(inputString, repNumber) {
+    if (repNumber<0) {
+        return "ERROR";
+    } else {
+        let outputString = "";
+            for (let i=0; i<repNumber; i++) {
+                outputString = outputString.concat(inputString);
+            }
+        return outputString;
+    }
+}
+
+repeatString("Hey",1);
 
 // Do not edit below this line
 module.exports = repeatString;
