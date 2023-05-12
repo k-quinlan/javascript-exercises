8388c1d6f1e78820b3692042f4601905253c6169
k-quinlan <132873356+k-quinlan@users.noreply.github.com>
2023-05-12 21:05:28 +1000

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let stringLength = inputString.length;
+    let myArray = Array.from(inputString);
+    let outputString = "";
+    for (let i = 0; i < stringLength; i++) {
+        outputString = outputString.concat(myArray.pop());
+    }
+    return outputString;
 };
+
+//reverseString("ABCDE");
 
 // Do not edit below this line
 module.exports = reverseString;
