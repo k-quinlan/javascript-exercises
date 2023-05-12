3c3c9178ab51a38d132df5ecd688a5874e532d59
k-quinlan <132873356+k-quinlan@users.noreply.github.com>
2023-05-12 23:22:07 +1000

    Exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  let resultTemp = (inputTemp-32) * (5/9);
+  //  return resultTemp.toFixed(1);
+  return Math.round(resultTemp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  let resultTemp = (inputTemp * (9/5) + 32);
+  // return resultTemp.toFixed(1);
+  return Math.round(resultTemp*10)/10;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
